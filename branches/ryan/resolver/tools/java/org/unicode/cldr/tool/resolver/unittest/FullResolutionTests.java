c1ff400b3494c18ccd5f1ba7d3872e29cc687398
ryan <ryan@b1474bd8-1051-4aae-843b-7d7c63456b9e>
2011-07-28 23:00:22 +0000

    cldrbug 3984: Special-cased out //ldml/identity, added canonical XPath support, Style fixes
    git-svn-id: https://unicode.org/repos/cldr@6043 b1474bd8-1051-4aae-843b-7d7c63456b9e

--- branches/ryan/resolver/tools/java/org/unicode/cldr/tool/resolver/unittest/FullResolutionTests.java
+++ branches/ryan/resolver/tools/java/org/unicode/cldr/tool/resolver/unittest/FullResolutionTests.java
@@ -16,6 +16,7 @@ import org.unicode.cldr.util.CLDRFile;
 import org.unicode.cldr.util.CLDRFile.Factory;
 import org.unicode.cldr.util.CldrUtility;
 import org.unicode.cldr.util.XMLFileReader;
+import org.unicode.cldr.util.XPathParts;
 import org.unicode.cldr.util.XMLFileReader.SimpleHandler;
 
 import com.ibm.icu.dev.test.TestFmwk;
@@ -28,7 +29,7 @@ import com.ibm.icu.dev.test.TestFmwk;
  * @author ryanmentley@google.com (Ryan Mentley)
  */
 public class FullResolutionTests extends TestFmwk {
-  private static final String LOCALES_TO_TEST = "am_ET";
+  private static final String LOCALES_TO_TEST = ".*";
   private static final ResolutionType RESOLUTION_TYPE = ResolutionType.FULL;
 
   public void TestFullResolution() {
@@ -40,56 +41,73 @@ public class FullResolutionTests extends TestFmwk {
           + "Set it using -DCLDR_DIR=/path/to/cldr");
       return;
     }
-    // If this is ever made multi-threaded, we should just make our own factories. 
+    // If this is ever made multi-threaded, we should just make our own
+    // factories.
     Factory factory = TestInfo.getInstance().getCldrFactory();
     Set<String> locales = resolver.getLocaleNames(LOCALES_TO_TEST);
     for (String locale : locales) {
       // Resolve with CLDR and with the tool
       CLDRFile cldrResolved = factory.make(locale, true);
       CLDRFile toolResolved = resolver.resolveLocale(locale, RESOLUTION_TYPE);
-      
+
       // Create sets to hold the paths from CLDR and the tool
       Set<String> cldrPaths = new HashSet<String>();
       Set<String> toolPaths = new HashSet<String>();
-      
+
       // Print the tool output to an XML String
       StringWriter stringOut = new StringWriter();
       PrintWriter pw = new PrintWriter(stringOut);
       toolResolved.write(pw);
       String xml = stringOut.toString();
-      
+
       // Read the XML string back in for testing
       StringReader sr = new StringReader(xml);
       XMLFileReader xmlReader = new XMLFileReader();
       xmlReader.setHandler(new TestHandler(cldrResolved, toolPaths));
       xmlReader
           .read(locale, sr, XMLFileReader.CONTENT_HANDLER | XMLFileReader.ERROR_HANDLER, false);
-      
-      // Check to make sure no paths from the CLDR resolved version that aren't aliases get left out
+
+      // Check to make sure no paths from the CLDR resolved version that aren't
+      // aliases get left out
       for (Iterator<String> fileIter = cldrResolved.iterator("", CLDRFile.ldmlComparator); fileIter
           .hasNext();) {
         String distinguishedPath = fileIter.next();
         String fullPath = cldrResolved.getFullXPath(distinguishedPath);
-        if (!distinguishedPath.endsWith("/alias")) {
-          assertTrue("Path " + fullPath + " is present in CLDR resolved file for locale " + locale
-              + " but not in tool resolved file.", toolPaths.contains(fullPath));
+        // Ignore aliases and the //ldml/identity/ elements
+        if (!distinguishedPath.endsWith("/alias") && !distinguishedPath.startsWith("//ldml/identity/")) {
+          String canonicalPath = canonicalXpath(fullPath);
+          assertTrue("Path " + canonicalPath + " is present in CLDR resolved file for locale " + locale
+              + " but not in tool resolved file.", toolPaths.contains(canonicalPath));
         }
-        
+
         // Add the path to the Set for the next batch of checks
-        cldrPaths.add(fullPath);
+        cldrPaths.add(canonicalXpath(fullPath));
       }
       for (Iterator<String> pathIter = toolPaths.iterator(); pathIter.hasNext();) {
         String fullPath = pathIter.next();
-        assertTrue("Path " + fullPath + " is present in tool resolved file for locale " + locale
-          + " but not in CLDR resolved file.", cldrPaths.contains(fullPath));
+        // Ignore the //ldml/identity/ elements
+        if (!fullPath.startsWith("//ldml/identity/")) {
+          assertTrue("Path " + fullPath + " is present in tool resolved file for locale " + locale
+              + " but not in CLDR resolved file.", cldrPaths.contains(fullPath));
+        }
       }
     }
   }
   
+  /**
+   * Returns a canonical representation of an XPath for use in comparing XPaths
+   * 
+   * @param xPath the original XPath
+   * @return the canonical representation of xPath
+   */
+  private String canonicalXpath(String xPath) {
+    return new XPathParts().initialize(xPath).toString();
+  }
+
   private class TestHandler extends SimpleHandler {
     CLDRFile file;
     Set<String> paths;
-    
+
     /**
      * Creates a test handler
      * 
@@ -100,11 +118,10 @@ public class FullResolutionTests extends TestFmwk {
       this.file = cldrResolvedFile;
       paths = pathSet;
     }
-    
+
     @Override
     public void handlePathValue(String path, String value) {
-      // System.out.println("Path: \"" + path + "\" Value: \"" + value + "\"");
-      paths.add(path);
+      paths.add(canonicalXpath(path));
       assertEquals("CLDRFile resolved value for " + path + " in locale " + file.getLocaleID()
           + " should match tool resolved value", file.getStringValue(path), value);
     }
