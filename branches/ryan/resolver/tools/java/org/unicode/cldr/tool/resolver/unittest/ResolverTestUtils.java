ed4c4bbe2a608ca74043e38b35de264addc78afa
ryan <ryan@b1474bd8-1051-4aae-843b-7d7c63456b9e>
2011-08-01 19:22:58 +0000

    cldrbug 3984: Added static helper class forgotten in previous commit
    git-svn-id: https://unicode.org/repos/cldr@6052 b1474bd8-1051-4aae-843b-7d7c63456b9e

---
+++ branches/ryan/resolver/tools/java/org/unicode/cldr/tool/resolver/unittest/ResolverTestUtils.java
@@ -0,0 +1,46 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+
+package org.unicode.cldr.tool.resolver.unittest;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.unicode.cldr.util.CLDRFile;
+import org.unicode.cldr.util.XPathParts;
+
+/**
+ * Package-private class containing helper methods for testing the CLDR resolver tool
+ *  
+ * @author ryanmentley@google.com (Ryan Mentley)
+ */
+class ResolverTestUtils {
+  
+  /**
+   * Static class, no public constructor
+   */
+  private ResolverTestUtils() {}
+
+  /**
+   * Prints a CLDRFile to a String
+   * 
+   * @param cldrFile the CLDRFile to print
+   * @return a String containing the data from cldrFile in XML
+   */
+  static String writeToString(CLDRFile cldrFile) {
+    StringWriter stringOut = new StringWriter();
+    PrintWriter pw = new PrintWriter(stringOut);
+    cldrFile.write(pw);
+    String xml = stringOut.toString();
+    return xml;
+  }
+  
+  /**
+   * Returns a canonical representation of an XPath for use in comparing XPaths
+   * 
+   * @param xPath the original XPath
+   * @return the canonical representation of xPath
+   */
+  static String canonicalXpath(String xPath) {
+    return new XPathParts().initialize(xPath).toString();
+  }
+}
