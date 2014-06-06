3ffa1842cca5871d37d615dd4c7a7cbc72d3dccf
ribnitz <ribnitz@b1474bd8-1051-4aae-843b-7d7c63456b9e>
2014-06-06 13:19:32 +0000

    cldrbug 7183: Merge changes from trunk, second part part (preparation for merging back to trunk)
    git-svn-id: https://unicode.org/repos/cldr@10500 b1474bd8-1051-4aae-843b-7d7c63456b9e

--- branches/ribnitz_perfwork_7183/tools/java/org/unicode/cldr/util/CLDRConfig.java
+++ branches/ribnitz_perfwork_7183/tools/java/org/unicode/cldr/util/CLDRConfig.java
@@ -33,6 +33,11 @@ public class CLDRConfig extends Properties {
     private static final Object CLDR_FACTORY_SYNC = new Object();
 
     /**
+     * Object to use for synchronization when interacting with Factory
+     */
+    private static final Object FULL_FACTORY_SYNC = new Object();
+
+    /**
      * Object used for synchronization when interacting with SupplementalData
      */
     private static final Object SUPPLEMENTAL_DATA_SYNC = new Object();
@@ -56,6 +61,11 @@ public class CLDRConfig extends Properties {
      * Object used for synchronization in getStandardCodes()
      */
     private static final Object GET_STANDARD_CODES_SYNC = new Object();
+    
+    /**
+     * Object used for synchronization in getCoverageInfo()
+     */
+    private static Object COVERAGE_INFO_SYNC=new Object(); 
 
     public enum Environment {
         LOCAL, // < == unknown.
@@ -114,12 +124,12 @@ public class CLDRConfig extends Properties {
     public String getInitStack() {
         return initStack;
     }
-
-    private static Object COVERAGE_INFO_SYNC=new Object();
+  
     private CoverageInfo coverageInfo=null;
     private SupplementalDataInfo supplementalDataInfo;
     private StandardCodes sc;
     private Factory cldrFactory;
+    private Factory fullFactory;
     private Factory supplementalFactory;
     private CLDRFile english;
     private CLDRFile root;
@@ -167,7 +177,6 @@ public class CLDRConfig extends Properties {
         return sc;
     }
 
-    
     public CoverageInfo getCoverageInfo() {
         synchronized(COVERAGE_INFO_SYNC) {
             if (coverageInfo==null) {
@@ -183,6 +192,16 @@ public class CLDRConfig extends Properties {
             }
         }
         return cldrFactory;
+    }
+
+    public Factory getFullCldrFactory() {
+        synchronized (FULL_FACTORY_SYNC) {
+            if (fullFactory == null) {
+                File[] paths = { new File(CLDRPaths.MAIN_DIRECTORY), new File(CLDRPaths.SEED_DIRECTORY) };
+                fullFactory = SimpleFactory.make(paths, ".*");
+            }
+        }
+        return fullFactory;
     }
 
     public Factory getSupplementalFactory() {
@@ -284,10 +303,18 @@ public class CLDRConfig extends Properties {
                 curEnvironment = Environment.valueOf(envString.trim());
             }
             if (curEnvironment == null) {
-                curEnvironment = Environment.LOCAL;
+                curEnvironment = getDefaultEnvironment();
             }
         }
         return curEnvironment;
+    }
+    
+    /**
+     * If no environment is defined, what is the default?
+     * @return
+     */
+    protected Environment getDefaultEnvironment() {
+        return Environment.LOCAL;
     }
 
     public void setEnvironment(Environment environment) {
