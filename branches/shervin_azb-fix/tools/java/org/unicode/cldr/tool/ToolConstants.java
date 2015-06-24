572769a90b47647aef2cea41341d89a307f48bd2
shervin <shervin@b1474bd8-1051-4aae-843b-7d7c63456b9e>
2015-06-24 15:39:55 +0000

    cldrbug 8437: Keeping the changes which had to reverted for future merge
    git-svn-id: https://unicode.org/repos/cldr@11729 b1474bd8-1051-4aae-843b-7d7c63456b9e

---
+++ branches/shervin_azb-fix/tools/java/org/unicode/cldr/tool/ToolConstants.java
@@ -0,0 +1,38 @@
+package org.unicode.cldr.tool;
+
+import org.unicode.cldr.util.CldrUtility;
+
+/**
+ * Constants specific to CLDR tools.
+ * Not to be used with the Survey Tool.
+ * Moved here from CldrUtilities
+ * @author srl
+ *
+ */
+public class ToolConstants {
+
+    // We are now having charts point to the appropriate source, so this may take some tweaking!
+    public enum ChartStatus {
+        beta, // at the start of the release
+        trunk, // before the release is tagged
+        release // for release version
+    }
+
+    // Change the following for each release depending on the phase
+
+    private static final String DEFAULT_CHART_VERSION = "28";
+    private static final ChartStatus DEFAULT_CHART_STATUS = ChartStatus.beta;
+
+    // DON'T CHANGE ANY OF THE FOLLOWING; THEY ARE DRIVEN BY THE ABOVE
+
+    // allows overriding with -D
+    public static final String CHART_VERSION = CldrUtility.getProperty("CHART_VERSION", DEFAULT_CHART_VERSION);
+    public static final ChartStatus CHART_STATUS = ChartStatus.valueOf(CldrUtility.getProperty("CHART_STATUS", DEFAULT_CHART_STATUS.toString()));
+    
+    // build from the above
+    public static final boolean BETA = CHART_STATUS == ChartStatus.beta;
+    public static final String LAST_CHART_VERSION = Integer.parseInt(CHART_VERSION) + ".0"; // must have 1 decimal
+    public static final String CHART_DISPLAY_VERSION = CHART_VERSION + (BETA ? "β" : "");
+    public static final String CHART_SOURCE = "http://unicode.org/repos/cldr/"
+        + (CHART_STATUS != ChartStatus.release ? "trunk/" : "tags/release-" + CHART_VERSION + "/");
+}
