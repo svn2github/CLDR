53074b4887c24fa6bee2eebd521e59fd1c832f03
emmons <emmons@b1474bd8-1051-4aae-843b-7d7c63456b9e>
2017-09-28 17:13:22 +0000

    cldrbug 10584: tag CLDR release-32-beta
    git-svn-id: https://unicode.org/repos/cldr@13749 b1474bd8-1051-4aae-843b-7d7c63456b9e

---
+++ tags/release-32-beta/tools/java/org/unicode/cldr/tool/GenerateDtd.java
@@ -0,0 +1,38 @@
+package org.unicode.cldr.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.unicode.cldr.draft.FileUtilities;
+import org.unicode.cldr.util.CLDRPaths;
+import org.unicode.cldr.util.DtdData;
+import org.unicode.cldr.util.DtdType;
+
+import com.ibm.icu.impl.CaseMapImpl;
+import com.ibm.icu.lang.UCharacter;
+import com.ibm.icu.util.ULocale;
+
+public class GenerateDtd {
+    
+    static final int FIRST_TITLE = CaseMapImpl.TITLECASE_WHOLE_STRING|UCharacter.TITLECASE_NO_LOWERCASE;
+
+    public static void main(String[] args) throws IOException {
+        //System.setProperty("show_all", "true");
+        for (DtdType type : DtdType.values()) {
+            if (type == DtdType.ldmlICU) {
+                continue;
+            }
+            DtdData data = DtdData.getInstance(type);
+            String name = type.toString();
+            if (!name.startsWith("ldml")) {
+                name = "ldml" + UCharacter.toTitleCase(ULocale.ENGLISH, name, null, FIRST_TITLE);
+                if (name.endsWith("Data")) {
+                    name = name.substring(0, name.length() - 4);
+                }
+            }
+            try (PrintWriter out = FileUtilities.openUTF8Writer(CLDRPaths.GEN_DIRECTORY + "dtd/", name + ".dtd")) {
+                out.println(data);
+            }
+        }
+    }
+}
