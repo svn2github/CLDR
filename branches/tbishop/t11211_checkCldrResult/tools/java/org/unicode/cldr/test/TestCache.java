36824460ba9bbb16d5f64f86eb5a8c28d51c1f77
tbishop <tbishop@b1474bd8-1051-4aae-843b-7d7c63456b9e>
2018-11-21 01:04:00 +0000

    cldrbug 11211: create checkCldrResult and examplesResult lists newly for each row instead of clearing and re-using; result.clear in TestResultBundle.check for further protection against phantom warnings; use ConcurrentHashMap, and ImmutableList.copyOf, instead of HashMap for pathCache; make some members private, final; remove unused code
    git-svn-id: https://unicode.org/repos/cldr@14656 b1474bd8-1051-4aae-843b-7d7c63456b9e

--- branches/tbishop/t11211_checkCldrResult/tools/java/org/unicode/cldr/test/TestCache.java
+++ branches/tbishop/t11211_checkCldrResult/tools/java/org/unicode/cldr/test/TestCache.java
@@ -1,14 +1,16 @@
 package org.unicode.cldr.test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.unicode.cldr.test.CheckCLDR.CheckStatus;
 import org.unicode.cldr.util.CLDRFile;
 import org.unicode.cldr.util.Factory;
 import org.unicode.cldr.util.Pair;
 import org.unicode.cldr.util.XMLSource;
+
+import com.google.common.collect.ImmutableList;
 
 /**
  * Caches tests
@@ -20,14 +22,14 @@ import org.unicode.cldr.util.XMLSource;
 public abstract class TestCache implements XMLSource.Listener {
     public class TestResultBundle {
         protected List<CheckStatus> possibleProblems = new ArrayList<CheckStatus>();
-        CLDRFile file;
-        CheckCLDR cc = createCheck();
-        private CheckCLDR.Options options;
-        private HashMap<Pair<String, String>, List<CheckStatus>> pathCache;
+        final CLDRFile file;
+        final private CheckCLDR cc = createCheck();
+        final private CheckCLDR.Options options;
+        final private ConcurrentHashMap<Pair<String, String>, List<CheckStatus>> pathCache;
         
         protected TestResultBundle(CheckCLDR.Options cldrOptions) {
             options = cldrOptions;
-            pathCache = new HashMap<Pair<String, String>, List<CheckStatus>>();
+            pathCache = new ConcurrentHashMap<Pair<String, String>, List<CheckStatus>>();
             file = getFactory().make(options.getLocale().getBaseName(), true);
             cc.setCldrFileToCheck(file, options, possibleProblems);
         }
@@ -36,22 +38,23 @@ public abstract class TestCache implements XMLSource.Listener {
             return possibleProblems;
         }
 
-        /*
-         * Check the current value for the given path
-         */
-        public void check(String path, List<CheckStatus> result) {
-            check(path, result, file.getStringValue(path));
-        }
-       
        /**
         * Check the given value for the given path, using this TestResultBundle for
         * options, pathCache and cc (CheckCLDR).
         * 
         * @param path the path
-        * @param result the list to which CheckStatus objects may be added
+        * @param result the list to which CheckStatus objects may be added; this function
+        *               clears any objects that might already be in it
         * @param value the value to be checked
         */
         public void check(String path, List<CheckStatus> result, String value) {
+            /*
+             * result.clear() is needed to avoid phantom warnings in the Info Panel, if we're called
+             * with non-empty result (leftover from another row) and we get cachedResult != null.
+             * cc.check() also calls result.clear() (at least as of 2018-11-20) so in that case it's
+             * currently redundant here. Clear it here unconditionally to be sure.
+             */
+            result.clear();
             Pair<String, String> key = new Pair<String, String>(path, value);
             List<CheckStatus> cachedResult = pathCache.get(key);
             if (cachedResult != null) {
@@ -59,7 +62,7 @@ public abstract class TestCache implements XMLSource.Listener {
             }
             else {
                 cc.check(path, file.getFullXPath(path), value, options, result);
-                pathCache.put(key, result);
+                pathCache.put(key, ImmutableList.copyOf(result));
             }
         }
 
@@ -70,7 +73,6 @@ public abstract class TestCache implements XMLSource.Listener {
 
     private Factory factory = null;
     private String nameMatcher = null;;
-    CLDRFile displayInformation = null;
 
     protected Factory getFactory() {
         return factory;
@@ -89,7 +91,6 @@ public abstract class TestCache implements XMLSource.Listener {
         }
         this.factory = factory;
         this.nameMatcher = nameMatcher;
-        this.displayInformation = displayInformation;
     }
 
     /**
